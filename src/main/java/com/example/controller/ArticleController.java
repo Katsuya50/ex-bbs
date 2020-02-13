5df45888fdfd66b430a825ed75e690f70c30926c
Katsuya50 <katsuya.fujishima@rakus-partners.co.jp>
2020-02-13 11:27:20 +0900

    既に投稿された内容を含んだ掲示板画面にフォワードするメソッドを作成しました。

--- src/main/java/com/example/controller/ArticleController.java
+++ src/main/java/com/example/controller/ArticleController.java
@@ -1,14 +1,22 @@
 package com.example.controller;
 
-import javax.servlet.ServletContext;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.domain.Article;
 import com.example.repository.ArticleRepository;
 
+/**
+ * 掲示板アプリのコントローラークラス.
+ * 
+ * @author katsuya.fujishima
+ *
+ */
 @Controller
 @Transactional
 @RequestMapping("/")
@@ -17,9 +25,17 @@ public class ArticleController {
 	@Autowired
 	private ArticleRepository repository;
 	
-	@Autowired
-	private ServletContext application;
-	
-	
+	/**
+	 * 既に投稿された内容を含んだ掲示板画面をフォワードするメソッド.
+	 * 
+	 * @param model リクエストスコープ
+	 * @return 掲示板画面
+	 */
+	@RequestMapping("")
+	public String index(Model model) {
+		List<Article> articleList = repository.findAll();
+		model.addAttribute("articleList", articleList);
+		return "bbs";
+	}
 
 }
