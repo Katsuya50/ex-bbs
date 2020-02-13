e8017680459265a3f2e04d9e3cae91fa5e595800
Katsuya50 <katsuya.fujishima@rakus-partners.co.jp>
2020-02-13 15:32:01 +0900

    記事を削除するメソッドを作成しました。

--- src/main/java/com/example/repository/ArticleRepository.java
+++ src/main/java/com/example/repository/ArticleRepository.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
@@ -27,9 +28,9 @@ public class ArticleRepository {
 	private NamedParameterJdbcTemplate template;
 
 	/**	記事テーブル */
-	private static final String TABLE_NAME_1 = "articles";
+	private static final String ARTICLES_TABLE = "articles";
 	/**	コメントテーブル */
-	private static final String TABLE_NAME_2 = "comments";
+	private static final String COMMENTS_TABLE = "comments";
 
 	/**	コメントをリスト化、記事ドメインに全てのプロパティを格納してリストとして返すエクストラクター */
 	private static final ResultSetExtractor<List<Article>> ARTICLE_RS_EXTRACTOR = (rs) -> {
@@ -74,8 +75,8 @@ public class ArticleRepository {
 	public List<Article> findAll() {
 		String sql = "SELECT a.id as id, a.name as name, a.content as content, "
 					+ "c.id as com_id, c.name as com_name, c.content as com_content, c.article_id as article_id "
-					+ "FROM " + TABLE_NAME_1 + " as a "
-					+ "LEFT OUTER JOIN " + TABLE_NAME_2 + " as c "
+					+ "FROM " + ARTICLES_TABLE + " as a "
+					+ "LEFT OUTER JOIN " + COMMENTS_TABLE + " as c "
 					+ "ON a.id = c.article_id ORDER BY a.id DESC, c.id DESC";
 		List<Article> articleList = template.query(sql, ARTICLE_RS_EXTRACTOR);
 		return articleList;
@@ -87,8 +88,19 @@ public class ArticleRepository {
 	 * @param article 記事
 	 */
 	public void insert(Article article) {
-		String sql = "INSERT INTO " + TABLE_NAME_1 + " (name, content) VALUES (:name, :content)";
+		String sql = "INSERT INTO " + ARTICLES_TABLE + " (name, content) VALUES (:name, :content)";
 		SqlParameterSource param = new BeanPropertySqlParameterSource(article);
+		template.update(sql, param);
+	}
+	
+	/**
+	 * 記事を削除するメソッド.
+	 * 
+	 * @param id id
+	 */
+	public void deleteById(int id) {
+		String sql = "DELETE FROM " + ARTICLES_TABLE + " WHERE id = :id";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
 		template.update(sql, param);
 	}
 
