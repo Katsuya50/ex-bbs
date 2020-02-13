6a7c565347f10c3d9c155e9bea2334d7e076a741
Katsuya50 <katsuya.fujishima@rakus-partners.co.jp>
2020-02-13 14:42:50 +0900

    insertメソッドを作成しました。

--- src/main/java/com/example/repository/CommentRepository.java
+++ src/main/java/com/example/repository/CommentRepository.java
@@ -1,8 +1,9 @@
 package com.example.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import com.example.domain.Comment;
@@ -15,15 +16,11 @@ public class CommentRepository {
 	
 	private static final String TABLE_NAME = "comments";
 	
-	private static final RowMapper<Comment> COMMENT_ROW_MAPPER = (rs, i) -> {
-		Comment comment = new Comment();
-		comment.setId(rs.getInt("id"));
-		comment.setName(rs.getString("name"));
-		comment.setContent(rs.getString("content"));
-		comment.setArticleId(rs.getInt("article_id"));
-		return comment;
-	};
-	
-	
+	public void insert(Comment comment) {
+		String sql = "INSERT INTO " + TABLE_NAME + " (name, content, article_id) "
+					+ "VALUES (:name, :content, :articleId)";
+		SqlParameterSource param = new BeanPropertySqlParameterSource(comment);
+		template.update(sql, param);
+	}
 
 }
